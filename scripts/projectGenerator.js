d5a6e8e7c1fcbea3311b0ab251878ed5ecee775b
Ardon Bailey <waystylez1@gmail.com>
2018-11-12 11:28:01 -0800

    made changes to script

--- scripts/projectGenerator.js
+++ scripts/projectGenerator.js
@@ -1,63 +1,53 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
+const ora = require('ora');
 const fs = require('fs');
+const fse = require('fs-extra');
+const { promisify } = require('util');
 
-const CHOICES = fs.readdirSync(`${__dirname}/../templates`);
-
-const QUESTIONS = [
-  {
-    name: 'project-choice',
-    type: 'list',
-    message: 'What project template would you like to generate?',
-    choices: CHOICES
-  },
-  {
-    name: 'project-name',
-    type: 'input',
-    message: 'Project name:',
-    validate: function(input) {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else
-        return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
-  }
-];
-
+const readdir = promisify(fs.readdir);
 const CURRENT_DIRECTORY = process.cwd();
 
-inquirer.prompt(QUESTIONS).then(answers => {
+async function copyDirectory(filePath) {
+  const response = await readdir(filePath);
+
+  const QUESTIONS = [
+    {
+      name: 'project-choice',
+      type: 'list',
+      message: 'What project template would you like to generate?',
+      choices: response
+    },
+    {
+      name: 'project-name',
+      type: 'input',
+      message: 'Project name:',
+      validate: function(input) {
+        if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+        else
+          return 'Project name may only include letters, numbers, underscores and hashes.';
+      }
+    }
+  ];
+
+  const answers = await inquirer.prompt(QUESTIONS);
   const projectChoice = answers['project-choice'];
   const projectName = answers['project-name'];
-  const templatePath = `${__dirname}/../templates/${projectChoice}`;
+  const templatePath = `${filePath}/${projectChoice}`;
 
   fs.mkdirSync(`${CURRENT_DIRECTORY}/${projectName}`);
-
   createDirectoryContents(templatePath, projectName);
-});
+}
 
-function createDirectoryContents(templatePath, newProjectPath) {
-  const filesToCreate = fs.readdirSync(templatePath);
+copyDirectory(`${__dirname}/../templates`);
 
-  filesToCreate.forEach(file => {
-    const origFilePath = `${templatePath}/${file}`;
-
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
-
-    if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, 'utf8');
-
-      const writePath = `${CURRENT_DIRECTORY}/${newProjectPath}/${file}`;
-      fs.writeFileSync(writePath, contents, 'utf8');
-    } else if (stats.isDirectory()) {
-      fs.mkdirSync(`${CURRENT_DIRECTORY}/${newProjectPath}/${file}`);
-
-      // recursive call
-      createDirectoryContents(
-        `${templatePath}/${file}`,
-        `${newProjectPath}/${file}`
-      );
-    }
-  });
+async function createDirectoryContents(templatePath, newProjectPath) {
+  try {
+    const spinner = ora('Generating project, hold up...').start();
+    await fse.copy(templatePath, newProjectPath);
+    spinner.succeed('All done');
+  } catch (error) {
+    console.error(error);
+  }
 }
